789d5885547a7e82ce7bad354b868ab147f0f5ae
Ani peter benjamin <99623072+vortex-hue@users.noreply.github.com>
2022-10-10 02:01:40 +0100

    added files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - a function that returns an int
+* return - 0 if executed properly
+*
+**/
+
+int main()
+{
+	printf("Hello World of C programming");
+	
+	return 0;
+}
+
---
+++ main2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - a function that returns the 
+* output of an inputted number
+* Return: 0
+*/
+
+int main()
+{
+    int num;
+ 
+    printf( "Please enter a number: " );
+    scanf( "%d", &num );
+    printf( "You entered %d\n", num );
+
+    return 0;
+}
